1917c03a22cb04fed5f391505b5dff8cccd97432
abgill <gill.andrew.95@gmail.com>
2017-10-02 23:29:50 +0000

    changed api

--- script.js
+++ script.js
@@ -61,7 +61,9 @@ $(document).ready(function () {
 
         var wordLength = $("#wordLen").val();
 
-        var myurl = "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=false&excludePartOfSpeech=family-name&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=" + wordLength + "&maxLength="+ wordLength + "&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
+        //var myurl = "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=false&excludePartOfSpeech=family-name&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=" + wordLength + "&maxLength="+ wordLength + "&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
+        var myurl = "http://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=true&excludePartOfSpeech=family-name,abbreviation,given-name,suffix,idiom&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength="+ wordLength + "&maxLength=" + wordLength + "&limit=1&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5";
+
         $.ajax({
             type: "GET",
             url : myurl,
